522dc3ccaf74f1d5ea1afb8cd51bd835ff6f4b72
Rezapu Obed <obed.ubaidillah@gmail.com>
2021-07-19 11:02:14 +0000

    get user id

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import os
-
+import discord
+import random
+from replit import db
 
 from keep_alive import keep_alive
 keep_alive()
 
-import discord
-
 client = discord.Client()
+db[user_id]={}
+db[list_id]=[]
+
+def get_user_id(author):
+  if db[user_id].get(author,-1)!=-1:
+    rand = random.randint(1000,9999)
+    while rand in db[list_id]:
+      rand=random.randint(1000,9999)
+    db[user_id][author]=rand
+  
+  return db[user_id][author]
 
 @client.event
 async def on_message(message):
@@ -14,7 +25,8 @@ async def on_message(message):
     return
 
   msg = message.content;
+  user_id = get_user_id(message.author)
 
-  await message.channel.purge()
+  await message.channel.send(user_id)
 
 client.run(os.getenv('TOKEN'))
