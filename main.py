4f33fabcc72489615d4f9811eb1c6f846ae4e680
Rezapu Obed <obed.ubaidillah@gmail.com>
2021-07-19 10:29:33 +0000

    basic

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+import os
+
+
 from keep_alive import keep_alive
 keep_alive()
 
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+  if message.author.bot and message.channel=="ngaku":
+    return
+
+  msg = message.content;
+
+  await message.channel.purge()
+
+client.run(os.getenv('TOKEN'))
