187f032cecd97f7f699a53233bf3bc4895b26518
Rezapu Obed <obed.ubaidillah@gmail.com>
2021-07-19 12:46:04 +0000

    working 1.0

--- main.py
+++ main.py
@@ -7,26 +7,33 @@ from keep_alive import keep_alive
 keep_alive()
 
 client = discord.Client()
-db[user_id]={}
-db[list_id]=[]
 
 def get_user_id(author):
-  if db[user_id].get(author,-1)!=-1:
+  if author not in db["user_id"].keys():
     rand = random.randint(1000,9999)
-    while rand in db[list_id]:
-      rand=random.randint(1000,9999)
-    db[user_id][author]=rand
-  
-  return db[user_id][author]
+    while rand in db["user_id"].values():
+      rand = random.randint(1000,9999)
+    db["user_id"][author]=rand
+  user_id = db["user_id"][author]
+
+  return user_id
+
+# def delete_all_message(message):
+#   for msg in message.channel:
+#     print(msg)
 
 @client.event
 async def on_message(message):
-  if message.author.bot and message.channel=="ngaku":
+  if message.author.bot or str(message.channel)!="ngaku":
     return
 
   msg = message.content;
-  user_id = get_user_id(message.author)
+  msgStripped = msg.strip("`")
+  user_id = get_user_id(str(message.author))
+  
+  # delete_all_message(message)
 
-  await message.channel.send(user_id)
+  await message.channel.purge(limit=1)
+  await message.channel.send(f"<{user_id}>: ```{msgStripped}```")
 
 client.run(os.getenv('TOKEN'))
