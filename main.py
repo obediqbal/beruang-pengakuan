2be1bd3af9f7ff3c7e1582689616bce01b8256a4
Rezapu Obed <obed.ubaidillah@gmail.com>
2021-07-21 17:56:01 +0000

    added Server object to db

--- main.py
+++ main.py
@@ -1,143 +1,232 @@
 import os
 import discord
 import random
+import json
+from types import SimpleNamespace
 from replit import db
 
 from keep_alive import keep_alive
+
 keep_alive()
 
 client = discord.Client()
 
-prefix=''
-authority=''
-commands={}
-dbUID = {}
+# prefix = ''
+# authority = ''
+# commands = {}
+# dbUID = {}
 # for key in db.keys():
 #   del db[key]
 
-def load_db():
-  # if "server" not in db.keys():
+default_prefix = '>'
+default_authority = 'Normal People'
+default_commands = {
+            'purge_messages': 'purgeMessages',
+            'change_user_ID': 'changeUID',
+            'change_bot_prefix': 'changePrefix',
+            'reset_user_id': 'resetUID',
+            'commands_list': 'help'
+            }
+default_userIds = {}
+         
+class Parser(object):
+  def __init__(self,obj):
+    self.__dict__ = json.loads(obj)
 
-  if "prefix" not in db.keys():
-    db["prefix"] = '>'
-  if "authority" not in db.keys():
-    db["authority"] = 'Normal People'
-  if "commands" not in db.keys():
-    db["commands"] = {
-      'purge_messages' : 'purgeMessages',
-      'change_user_ID' : 'changeUID',
-      'change_bot_prefix' : 'changePrefix',
-      'reset_user_id' : 'resetUID',
-      'commands_list' : 'help'
-    }
-  if "user_id" not in db.keys():
-    db["user_id"] = {}
-  global prefix, authority, commands, dbUID
-  prefix = db["prefix"]
-  authority = db["authority"]
-  commands = db["commands"]
-  dbUID = db["user_id"]
+class Server:
+  def __init__(self,prefix=default_prefix,authority=default_authority,commands=default_commands,userIds=default_userIds):
+    self.prefix = prefix
+    self.authority = authority
+    self.commands = commands
+    self.userIds = userIds
 
-def save_to_db(key,changes):
-  del db[key]
-  db[key]=changes
+def generate_user_id(serverObject, author):
+  author = str(author.id)
+  if author not in serverObject.userIds.keys():
+    rand = random.randint(0000, 9999)
+    while rand in serverObject.userIds.values():
+      rand = random.randint(0000, 9999)
+    serverObject.userIds[author] = rand
 
-def get_user_id(author):
-  author = str(author)
-  if author not in dbUID.keys():
-    rand = random.randint(0000,9999)
-    while rand in dbUID.values():
-      rand = random.randint(0000,9999)
-    dbUID[author]=rand
-  user_id = dbUID[author]
+  return serverObject
 
-  return user_id
 
-def reset_user_id(author):
-  author = str(author)
-  del dbUID[author]
+def reset_user_id(serverObject, author):
+  del serverObject.userIds[str(author.id)]
+  serverObject = generate_user_id(serverObject, author)
+  
+  return serverObject
 
-def change_user_id_to(author,num):
-  author = str(author)
-  reset_user_id(author)
-  dbUID[author]=num
+
+def change_user_id_to(serverObject, author, num):
+  author = str(author.id)
+  del serverObject.userIds[author]
+  serverObject.userIds[author] = num
+
+  return serverObject
+
+
+def load_server(serverid):
+    if "server" not in db.keys():
+      db["server"] = {}
+    if serverid not in db["server"].keys():
+      db["server"][serverid] = json.dumps(Server().__dict__)
+
+def update_server(serverid,serverObject):
+  del db["server"][serverid]
+  db["server"][serverid]=json.dumps(serverObject.__dict__)
+
+# def load_db(serverid):
+#     if "prefix" not in db["server"][serverid].keys():
+#         db["server"][serverid]["prefix"] = '>'
+#     if "authority" not in db["server"][serverid].keys():
+#         db["server"][serverid]["authority"] = 'Normal People'
+#     if "commands" not in db["server"][serverid].keys():
+#         db["server"][serverid]["commands"] = {
+#             'purge_messages': 'purgeMessages',
+#             'change_user_ID': 'changeUID',
+#             'change_bot_prefix': 'changePrefix',
+#             'reset_user_id': 'resetUID',
+#             'commands_list': 'help'
+#         }
+#     if "user_id" not in db["server"][serverid].keys():
+#         db["server"][serverid]["user_id"] = {}
+#     global prefix, authority, commands, dbUID
+#     prefix = db["server"][serverid]["prefix"]
+#     authority = db["server"][serverid]["authority"]
+#     commands = db["server"][serverid]["commands"]
+#     dbUID = db["server"][serverid]["user_id"]
+
+
+# def save_to_db(serverid,key, changes):
+#     del db["server"][serverid][key]
+#     db["server"][serverid][key] = changes
+
+
+# def get_user_id(author, serverObject):
+#     author = author.name
+#     if author not in dbUID.keys():
+#         rand = random.randint(0000, 9999)
+#         while rand in dbUID.values():
+#             rand = random.randint(0000, 9999)
+#         dbUID[author] = rand
+#     user_id = dbUID[author]
+
+#     return user_id, dbUID
+
+
+# def reset_user_id(author,dbUID):
+#     author = str(author)
+#     del dbUID[author]
+    
+
+
+# def change_user_id_to(author, num):
+#     author = str(author)
+#     reset_user_id(author)
+#     dbUID[author] = num
+
 
 def add_extra_digit(num):
-  temp = ""
-  for i in range(4-len(str(num))):
-    temp+="0"
-  temp+=str(num)
-  return temp
+    temp = ""
+    for i in range(4 - len(str(num))):
+        temp += "0"
+    temp += str(num)
+    return temp
 
-def find_role(role,author):
-  roles = map(str,author.roles)
-  for i in roles:
-    if role in i:
-      return True
-  return False
+
+def find_role(role, author):
+    roles = map(str, author.roles)
+    for i in roles:
+        if role in i:
+            return True
+    return False
+
+
 #Try changing boolean into int
+
 
 @client.event
 async def on_ready():
-  load_db()
-  await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening,name="your confession"))
+    await client.change_presence(activity=discord.Activity(
+        type=discord.ActivityType.listening, name="your confession"))
+
 
 @client.event
 async def on_message(message):
+    await client.change_presence(activity=discord.Activity(
+        type=discord.ActivityType.listening, name="your confession"))
 
-  await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening,name="your confession"))
-  if str(message.channel)!="ngaku":
-    if not str(message.channel).startswith("Direct Message"):
-      return
-  if message.author.bot:
-    return
+    DM = False
+    if str(message.channel) != "ngaku":
+        if not str(message.channel).startswith("Direct Message"):
+            return
+        DM = True
+    if message.author.bot:
+        return
 
-  msg = message.content
-  atchs = message.attachments
-  author = message.author
-  user_id = add_extra_digit(get_user_id(author.id))
+    msg = message.content
+    atchs = message.attachments
+    author = message.author
+    serverid = str(message.channel.id)
 
-  if msg.startswith(prefix):
-    args = [] 
-    try:
-      args = str(msg).split()[1:]
-    finally:
-      try:
-        if find_role(authority, author):
-          if msg.startswith(prefix+commands['purge_messages']):
-            await message.channel.purge()
-          elif msg.startswith(prefix+commands['change_user_ID']) and len(args):
-            if 0<int(args[0]) and int(args[0])<=9999 and int(args[0]) not in dbUID.values():
-              change_user_id_to(author.id, int(args[0]))
-          elif msg.startswith(prefix+commands['change_bot_prefix']):
-            save_to_db("prefix",args[0])
-            load_db()
-        if msg.startswith(prefix+commands['reset_user_id']):
-          reset_user_id(author.id)
-        elif msg.startswith(prefix+commands['commands_list']):
-          s = "```"
-          for command in commands:
-            s+=f"{command} : {commands[command]}\n"
-          s+=f"prefix : {prefix}```"
-          await message.channel.send(s)
-            
-      
-      finally:
+    load_server(serverid)
+    serverObject = Parser(db["server"][serverid])
+    prefix = serverObject.prefix
+    commands = serverObject.commands
+    authority = serverObject.authority
+
+    if str(author.id) not in serverObject.userIds.keys():
+      serverObject = generate_user_id(serverObject, author)
+
+    user_id = add_extra_digit(serverObject.userIds[str(author.id)])
+
+    if msg.startswith(prefix):
+        args = []
         try:
-          await message.delete()
+            args = str(msg).split()[1:]
         finally:
-          return
+            try:
+                if find_role(authority, author):
+                    if msg.startswith(prefix + commands['purge_messages']):
+                        await message.channel.purge()
+                    elif msg.startswith(prefix + commands['change_user_ID']) and len(args) :
+                        if 0 < int(args[0]) and int(args[0]) <= 9999 and int(args[0]) not in serverObject.userIds.values():
+                            serverObject = change_user_id_to(serverObject,author, int(args[0]))
+                    elif msg.startswith(prefix +
+                                        commands['change_bot_prefix']):
+                        serverObject.prefix=args[0]
+                        update_server(serverid,serverObject)
+                if msg.startswith(prefix + commands['reset_user_id']):
+                    serverObject = reset_user_id(serverObject,author)
+                    update_server(serverid,serverObject)
+                elif msg.startswith(prefix + commands['commands_list']):
+                    s = "```"
+                    for command in commands:
+                        s += f"{command} : {commands[command]}\n"
+                    s += f"prefix : {prefix}```"
+                    await message.channel.send(s)
 
-  try:
-    pass
-    await message.delete()
-  finally:
-    await message.channel.send(content=f"`<{user_id}>`: {msg}",reference=message.reference)
-    for atch in atchs:
-      await message.channel.send(atch)
+            finally:
+                try:
+                    if not DM:
+                      await message.delete()
+                finally:
+                    update_server(serverid, serverObject)
+                    return
+
+    try:
+        if not DM:
+          await message.delete()
+    finally:
+        update_server(serverid, serverObject)
+        await message.channel.send(content=f"`<{user_id}>`: {msg}",
+                                   reference=message.reference)
+        for atch in atchs:
+            await message.channel.send(atch)
+
 
 client.run(os.getenv('TOKEN'))
 
 # TODO:
 #   CONFESS VIA DM
-#   DIFFERENT ID PER SERVER
